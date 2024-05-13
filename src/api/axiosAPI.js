f7b6367cca5c0eaf616447b9c11e5db9d5f1f2d1
abdalla_mmdouh <abdallammdouh6@gmail.com>
2024-05-13 15:17:21 +0300

    edit in API_URL

--- src/api/axiosAPI.js
+++ src/api/axiosAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://cv-portal-api.onrender.com/api';
+const API_URL = 'https://cv-portal-api.onrender.com/api';
 
 export const axiosAPI = axios.create({
     baseURL : API_URL
