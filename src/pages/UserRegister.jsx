0886e95335ce380c582cdc753bc618c865ca39a3
abdalla_mmdouh <abdallammdouh6@gmail.com>
2024-05-13 16:14:41 +0300

    add redirect file

--- src/pages/UserRegister.jsx
+++ src/pages/UserRegister.jsx
@@ -311,6 +311,7 @@ const UserRegister = () => {
                   <input
                     placeholder="Software Engineer"
                     type="text"
+                    value={title}
                     onChange={(e) => setTitle(e.target.value)}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                   />
