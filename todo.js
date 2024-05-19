426d125a6798b068cff29df08dc4dfe26cbce4c6
abdalla_mmdouh <abdallammdouh6@gmail.com>
2024-05-19 03:30:35 +0300

    edit API URL

--- todo.js
+++
@@ -1,9 +0,0 @@
-/***
- 1 - SEARCH
- 2 - JOB VIEW JOB APPLICATIONS & UPDATE THE APPLICATION STATUS FOR THE USER 
- 3 - UPDATE PROFILE
- 4 - ABOUT PAGE
- 5 - make the update for the company and the user 
- 6 - make encryption and decryption for password
- 7 - add the dummy data
- */
